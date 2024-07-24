3fe4009e4a7f186b6a177eea56fe70e3563d3d00
Skanda <skanda9611@gmail.com>
2024-07-24 13:19:18 +0530

    Update and rename numpy.py to empty file

--- numpy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-arr = np.array([1,2,3,4])
-print("Array with rank 1: \n", arr)
-arr = np.array([[1,2,3],
-                [4,5,6]])
-print("Array with rank 2: \n", arr)
-arr = np.array((1,2,3))
-print("Array created using passed tuple: \n", arr)
